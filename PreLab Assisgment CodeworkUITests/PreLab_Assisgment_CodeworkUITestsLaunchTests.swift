50171fda74bef70de036948f1876933b521c76c2
adr577 <adrian003hernandez@gmail.com>
2024-02-02 21:23:09 -0800

    Initial Commit

---
+++ PreLab Assisgment CodeworkUITests/PreLab_Assisgment_CodeworkUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PreLab_Assisgment_CodeworkUITestsLaunchTests.swift
+//  PreLab Assisgment CodeworkUITests
+//
+//  Created by Adrian Hernandez on 2/2/24.
+//
+
+import XCTest
+
+final class PreLab_Assisgment_CodeworkUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
